a0969a14d3b9686c959ee466eb19dd60e68c5515
lishiju <425774863@qq.com>
2019-09-21 16:25:56 +0800

    first commit

---
+++ Example/Pods/Target Support Files/LSJSwiftSpecs/LSJSwiftSpecs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LSJSwiftSpecs : NSObject
+@end
+@implementation PodsDummy_LSJSwiftSpecs
+@end
---
+++ Example/Pods/Target Support Files/Pods-LSJSwiftSpecs_Tests/Pods-LSJSwiftSpecs_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LSJSwiftSpecs_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LSJSwiftSpecs_Tests
+@end
